20a0f48a15ca883f0356133e26da4f7215c6fb2f
iN1k1 <niki.martinel@gmail.com>
2017-12-14 11:32:13 +0100

    Customized cnn layer viz

--- src/cnn_layer_visualization.py
+++ src/cnn_layer_visualization.py
@@ -4,14 +4,14 @@ Created on Sat Nov 18 23:12:08 2017
 @author: Utku Ozbulak - github.com/utkuozbulak
 """
 import os
-import cv2
+from PIL import Image
 import numpy as np
 
 import torch
 from torch.optim import SGD
 from torchvision import models
 
-from misc_functions import preprocess_image, recreate_image
+from .misc_functions import preprocess_image, recreate_image
 
 
 class CNNLayerVisualization():
@@ -19,17 +19,24 @@ class CNNLayerVisualization():
         Produces an image that minimizes the loss of a convolution
         operation for a specific layer and filter
     """
-    def __init__(self, model, selected_layer, selected_filter):
+    def __init__(self, model, selected_layer, selected_filter,
+                 network_mean=(0.485, 0.456, 0.406), network_std=(0.229, 0.224, 0.225),
+                 im_size=(224,224,3),
+                 save_path='generated', use_gpu=False):
         self.model = model
         self.model.eval()
         self.selected_layer = selected_layer
         self.selected_filter = selected_filter
         self.conv_output = 0
         # Generate a random image
-        self.created_image = np.uint8(np.random.uniform(150, 180, (224, 224, 3)))
+        self.created_image = np.uint8(np.random.uniform(150, 180, im_size))
+        self.use_gpu = use_gpu
+        self.network_mean = network_mean
+        self.network_std = network_std
         # Create the folder to export images if not exists
-        if not os.path.exists('../generated'):
-            os.makedirs('../generated')
+        self.save_path = save_path
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
 
     def hook_layer(self):
         def hook_function(module, grad_in, grad_out):
@@ -43,7 +50,7 @@ class CNNLayerVisualization():
         # Hook the selected layer
         self.hook_layer()
         # Process image and return variable
-        self.processed_image = preprocess_image(self.created_image)
+        self.processed_image = preprocess_image(self.created_image, cudify=self.use_gpu, mean=self.network_mean, std=self.network_std)
         # Define optimizer for the image
         # Earlier layers need higher learning rates to visualize whereas later layers need less
         optimizer = SGD([self.processed_image], lr=5, weight_decay=1e-6)
@@ -69,20 +76,19 @@ class CNNLayerVisualization():
             # Update image
             optimizer.step()
             # Recreate image
-            self.created_image = recreate_image(self.processed_image)
+            self.created_image = recreate_image(self.processed_image.cpu(), mean=self.network_mean, std=self.network_std)
             # Save image
             if i % 5 == 0:
-                cv2.imwrite('../generated/layer_vis_l' + str(self.selected_layer) +
-                            '_f' + str(self.selected_filter) + '_iter'+str(i)+'.jpg',
-                            self.created_image)
+                Image.fromarray(self.created_image).save( os.path.join(self.save_path, 'layer_vis_l' + str(self.selected_layer) +
+                                 '_f' + str(self.selected_filter) + '_iter' + str(i) + '.jpg'))
 
-    def visualise_layer_without_hooks(self):
+    def visualise_layer_without_hooks(self, save_iter=5, num_iter=50):
         # Process image and return variable
-        self.processed_image = preprocess_image(self.created_image)
+        self.processed_image = preprocess_image(self.created_image, resize_im=False, cudify=self.use_gpu, mean=self.network_mean, std=self.network_std)
         # Define optimizer for the image
         # Earlier layers need higher learning rates to visualize whereas later layers need less
         optimizer = SGD([self.processed_image], lr=5, weight_decay=1e-6)
-        for i in range(1, 51):
+        for i in range(1, num_iter+1):
             optimizer.zero_grad()
             # Assign create image to a variable to move forward in the model
             x = self.processed_image
@@ -101,24 +107,23 @@ class CNNLayerVisualization():
             self.conv_output = x[0, self.selected_filter]
             # Loss function is the mean of the output of the selected layer/filter
             # We try to minimize the mean of the output of that specific filter
-            loss = torch.mean(self.conv_output)
+            loss = torch.mean(self.conv_output.cpu())
             print('Iteration:', str(i), 'Loss:', "{0:.2f}".format(loss.data.numpy()[0]))
             # Backward
             loss.backward()
             # Update image
             optimizer.step()
             # Recreate image
-            self.created_image = recreate_image(self.processed_image)
+            self.created_image = recreate_image(self.processed_image.cpu(), mean=self.network_mean, std=self.network_std)
             # Save image
-            if i % 5 == 0:
-                cv2.imwrite('../generated/layer_vis_l' + str(self.selected_layer) +
-                            '_f' + str(self.selected_filter) + '_iter'+str(i)+'.jpg',
-                            self.created_image)
+            if i % save_iter == 0:
+                Image.fromarray(self.created_image).save(os.path.join(self.save_path, 'layer_vis_l' + str(self.selected_layer) +
+                                         '_f' + str(self.selected_filter) + '_iter' + str(i) + '.jpg'))
 
 
 if __name__ == '__main__':
-    cnn_layer = 17
-    filter_pos = 0
+    cnn_layer = 2
+    filter_pos = 1
     # Fully connected layer is not needed
     pretrained_model = models.vgg16(pretrained=True).features
     layer_vis = CNNLayerVisualization(pretrained_model, cnn_layer, filter_pos)
